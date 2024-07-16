e37e509f65ac4ed8252698ea55feb280d7e9e81b
rhjdvsgsgks <26178113+rhjdvsgsgks@users.noreply.github.com>
2024-07-16 20:23:46 +0000

    Update build-android-arm64-v8a.sh

--- build-android-arm64-v8a.sh
+++ build-android-arm64-v8a.sh
@@ -92,7 +92,7 @@ cmake -DCMAKE_TOOLCHAIN_FILE="$ANDROID_NDK/build/cmake/android.toolchain.cmake" 
     -DSHERPA_ONNX_ENABLE_C_API=$SHERPA_ONNX_ENABLE_C_API \
     -DCMAKE_INSTALL_PREFIX=./install \
     -DANDROID_ABI="arm64-v8a" \
-    -DANDROID_PLATFORM=android-21 ..
+    -DANDROID_PLATFORM=android-27 ..
 
 # Please use -DANDROID_PLATFORM=android-27 if you want to use Android NNAPI
 
